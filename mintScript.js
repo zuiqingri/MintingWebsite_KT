424a0822aa087f0c9b74a7230fe78c5959b1564a
cchostar <ccho5384727@gmail.com>
2022-03-30 07:07:11 -0700

    Simple function

--- mintScript.js
+++ mintScript.js
@@ -12,7 +12,7 @@ function cntBlockNumber() {
     if(!blockCnt) {
         setInterval(function(){
             blockNumber+=1;
-            document.getElementById("blockNubmer").innerHTML = "현재 블록: #" + blockNumber;
+            document.getElementById("blockNubmer").innerHTML = "Current Block: #" + blockNumber;
         }, 1000);
         blockCnt = true;
     }
@@ -21,18 +21,18 @@ function cntBlockNumber() {
 async function connect() {
     const accounts = await klaytn.enable();
     if (klaytn.networkVersion === 8217) {
-        console.log("메인넷");
+        console.log("Mainnet");
     } else if (klaytn.networkVersion === 1001) {
-        console.log("테스트넷");
+        console.log("Testnet");
     } else {
-        alert("ERROR: 클레이튼 네트워크로 연결되지 않았습니다!");
+        alert("ERROR: Klaytn network not connected!");
         return;
     }
     account = accounts[0];
     caver.klay.getBalance(account)
         .then(function (balance) {
-            document.getElementById("myWallet").innerHTML = `지갑주소: ${account}`
-            document.getElementById("myKlay").innerHTML = `잔액: ${caver.utils.fromPeb(balance, "KLAY")} KLAY`
+            document.getElementById("myWallet").innerHTML = `Wallet address: ${account}`
+            document.getElementById("myKlay").innerHTML = `balance: ${caver.utils.fromPeb(balance, "KLAY")} KLAY`
         });
     await check_status();
 }
@@ -48,30 +48,30 @@ async function check_status() {
             maxSaleAmount = result[5];
             mintPrice = result[6];
             document.getElementById("mintCnt").innerHTML = `${mintIndexForSale - 1} / ${maxSaleAmount}`;
-            document.getElementById("mintLimitPerBlock").innerHTML = `트랜잭션당 최대 수량: ${mintLimitPerBlock}개`;
+            document.getElementById("mintLimitPerBlock").innerHTML = `Max amount per transaction: ${mintLimitPerBlock}`;
             document.getElementById('amount').max = mintLimitPerBlock;
-            document.getElementById("mintStartBlockNumber").innerHTML = `민팅 시작 블록: #${mintStartBlockNumber}`;
-            document.getElementById("mintPrice").innerHTML = `민팅 가격: ${caver.utils.fromPeb(mintPrice, "KLAY")} KLAY`;
+            document.getElementById("mintStartBlockNumber").innerHTML = `Minting start block: #${mintStartBlockNumber}`;
+            document.getElementById("mintPrice").innerHTML = `Minting price: ${caver.utils.fromPeb(mintPrice, "KLAY")} KLAY`;
         })
         .catch(function (error) {
             console.log(error);
         });
     blockNumber = await caver.klay.getBlockNumber();
-    document.getElementById("blockNubmer").innerHTML = "현재 블록: #" + blockNumber;
+    document.getElementById("blockNubmer").innerHTML = "Curent block: #" + blockNumber;
     cntBlockNumber();
 }
 
 async function publicMint() {
     if (klaytn.networkVersion === 8217) {
-        console.log("메인넷");
+        console.log("Mainnet");
     } else if (klaytn.networkVersion === 1001) {
-        console.log("테스트넷");
+        console.log("Testnet");
     } else {
-        alert("ERROR: 클레이튼 네트워크로 연결되지 않았습니다!");
+        alert("ERROR: Klaytn network not connected!");
         return;
     }
     if (!account) {
-        alert("ERROR: 지갑을 연결해주세요!");
+        alert("ERROR: Please connect your wallet!");
         return;
     }
 
@@ -79,10 +79,10 @@ async function publicMint() {
     const amount = document.getElementById('amount').value;
     await check_status();
     if (maxSaleAmount + 1 >= mintIndexForSale) {
-        alert("모든 물량이 소진되었습니다.");
+        alert("All stocks have been exhausted.");
         return;
     } else if (blockNumber <= mintStartBlockNumber) {
-        alert("아직 민팅이 시작되지 않았습니다.");
+        alert("Minting hasn't started yet.");
     }
     const total_value = amount * mintPrice;
 
@@ -113,15 +113,15 @@ async function publicMint() {
                     console.log(transferEvent);
                 })
                 .once("receipt", (receipt) => {
-                    alert("민팅에 성공하였습니다.");
+                    alert("Minting success.");
                 })
                 .on("error", (error) => {
-                    alert("민팅에 실패하였습니다.");
+                    alert("Minting failed.");
                     console.log(error);
                 });
         })
         .catch(function (error) {
             console.log(error);
-            alert("민팅에 실패하였습니다.");
+            alert("Minting failed.");
         });
 }
